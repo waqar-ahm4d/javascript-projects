1236d60f913b2160aa578ec4d10b0d1c1982ff54
Waqar Ahmad <thewaqarofficial@gmail.com>
2026-07-16 07:19:50 +0500

    Updated folder name

---
+++ image-search-app/data.js
@@ -0,0 +1,95 @@
+const imagesData = [
+    {
+        id: 1,
+        title: "Neon Cyberpunk Streets",
+        category: "tech",
+        url: "https://images.unsplash.com/photo-1784039798906-e00bd48d9f73?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8", // Landscape style
+        tags: ["tech", "cyberpunk", "neon"]
+    },
+    {
+        id: 2,
+        title: "Mist in Alpine Mountains",
+        category: "nature",
+        url: "https://picsum.photos/id/29/500/350",
+        tags: ["nature", "mountains", "fog"]
+    },
+    {
+        id: 3,
+        title: "Minimalist Coding Setup",
+        category: "tech",
+        url: "https://picsum.photos/id/60/500/350",
+        tags: ["tech", "code", "minimalist"]
+    },
+    {
+        id: 4,
+        title: "Autumn Forest Leaves",
+        category: "nature",
+        url: "https://picsum.photos/id/28/500/350",
+        tags: ["nature", "forest", "autumn"]
+    },
+    {
+        id: 5,
+        title: "Vintage Sports Car",
+        category: "retro",
+        url: "https://picsum.photos/id/111/500/350",
+        tags: ["retro", "cars", "vintage"]
+    },
+    {
+        id: 6,
+        title: "Mist in Alpine Mountains",
+        category: "nature",
+        url: "https://picsum.photos/id/29/500/350",
+        tags: ["nature", "mountains", "fog"]
+    },
+    {
+        id: 7,
+        title: "Minimalist Coding Setup",
+        category: "tech",
+        url: "https://picsum.photos/id/60/500/350",
+        tags: ["tech", "code", "minimalist"]
+    },
+    {
+        id: 8,
+        title: "Autumn Forest Leaves",
+        category: "nature",
+        url: "https://picsum.photos/id/28/500/350",
+        tags: ["nature", "forest", "autumn"]
+    },
+    {
+        id: 9,
+        title: "Vintage Sports Car",
+        category: "retro",
+        url: "https://picsum.photos/id/111/500/350",
+        tags: ["retro", "cars", "vintage"]
+    },
+    {
+        id: 10,
+        title: "Mist in Alpine Mountains",
+        category: "nature",
+        url: "https://picsum.photos/id/29/500/350",
+        tags: ["nature", "mountains", "fog"]
+    },
+    {
+        id: 11,
+        title: "Minimalist Coding Setup",
+        category: "tech",
+        url: "https://picsum.photos/id/11/500/350",
+        tags: ["tech", "code", "minimalist"]
+    },
+    {
+        id: 12,
+        title: "Autumn Forest Leaves",
+        category: "nature",
+        url: "https://picsum.photos/id/12/500/350",
+        tags: ["nature", "forest", "autumn"]
+    },
+    {
+        id: 13,
+        title: "Vintage Sports Car",
+        category: "retro",
+        url: "https://picsum.photos/id/113/500/350",
+        tags: ["retro", "cars", "vintage"]
+    }
+];
+
+export default imagesData;
